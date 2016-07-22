eae9f99a3457b8f3eb15e2998e06f242530a5837
Asha <asha@softstackfactory.org>
2016-07-22 23:21:02 +0000

    final v2 fixing scrollbar

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'RESTSer
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
